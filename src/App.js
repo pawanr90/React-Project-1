da49f441b6c78c12356bf5f0ecac4b500e6d9d30
Pawan Ramaswamy <pawanramaswamy@Pawans-MacBook-Pro.local>
2023-06-05 01:29:29 -0700

    Initial commit with all the files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'bulma/css/bulma.css'
+import ProfileCard from "./ProfileCard";
+import AlexaImage from './images/alexa.png'
+import CortanaImage from './images/cortana.png'
+import SiriImage from './images/siri.png'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <section className='hero is-primary'>
+                <div className='hero-body'>
+                    <p className='title'>Personal Digital Assistants</p>
+                </div>
+            </section>
+
+            <div className='container'>
+                <section className='section'>
+                    <div className='columns'>
+                        <div className='column is-4'>
+                            <ProfileCard 
+                                title="Alexa" 
+                                handle="@alexa99" 
+                                image={AlexaImage} 
+                                content="Alexa was created by Amazon and helps you with any basic questions"
+                            />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard 
+                                title="Cortana" 
+                                handle="@cortana32" 
+                                image={CortanaImage} 
+                                content="Cortana was made by Microsoft, but is not as popular as the other digital assistants"
+                            />
+                        </div>
+                        <div className='column is-4'>
+                            <ProfileCard 
+                                title="Siri" 
+                                handle="@siri01" 
+                                image={SiriImage}
+                                content="Siri was made by Apple and is a great chatting assistant"
+                            />
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/ProfileCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+function ProfileCard({title, handle, image, content}) {
+    return (
+        <div className='card'>
+            <div className='card-image'>
+                <figure className='image is-1by1'>
+                    <img src={image} alt="pda logo"/> 
+                </figure>
+            </div>
+
+            <div className='card-content'>
+                <div className='media-content'>
+                    <p className='title is-4'>{title}</p>
+                    <p className='title is-6'>{handle}</p>
+                </div>
+                <div className='content'>{content}</div>
+            </div>
+        </div>
+    );
+}
+
+export default ProfileCard
